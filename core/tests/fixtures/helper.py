3522e0b658a0a03850805eaf0c58521a9908683c
Asafe Oliveira <asafe@grito.cc>
2018-08-21 14:49:21 -0300

    develop - adc teste unitarios

---
+++ core/tests/fixtures/helper.py
@@ -0,0 +1,79 @@
+import pytest
+import random as rand
+
+from string import ascii_letters
+from core.db.model import Base, Product
+from pika import BlockingConnection, ConnectionParameters
+from random import choice
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy_utils import drop_database, create_database
+
+
+url_db = 'sqlite:///captura.db'
+
+
+def create_db_test():
+    create_database(url_db)
+    session = sessionmaker()
+    engine = create_engine(url_db)
+    session.configure(bind=engine)
+    Base.metadata.create_all(engine)
+
+    return engine
+
+
+@pytest.yield_fixture
+def gen_engine():
+
+    _engine = create_db_test()
+
+    yield _engine
+
+    drop_database(url_db)
+
+
+def get_engine_test():
+    return create_engine(url_db)
+
+
+def random_string(size=10):
+    return ''.join(choice(ascii_letters) for _ in range(size))
+
+
+def rand_int():
+    return rand.random()
+
+
+def gen_product(_id=None, url=None, title=None, name=None, status='WAIT'):
+    product = {
+        'id': _id if _id else rand_int(),
+        'url': url if url else random_string(12),
+    }
+    if title:
+        product['title'] = title
+    if name:
+        product['name'] = name
+    if status:
+        product['status'] = status
+
+    return product
+
+
+def insert_products(products, engine):
+    session_db = Session(bind=engine)
+    for product in products:
+        p = Product(**product)
+        session_db.add(p)
+        session_db.commit()
+
+
+# def connection_queue(queue='test'):
+#     connection = BlockingConnection(ConnectionParameters(host='localhost'))
+#     return connection
+
+
+# def close_conn_queue(connection, queue='test'):
+#     channel = connection.channel()
+#     channel.queue_delete(queue)
+#     connection.close()
---
+++ core/tests/test_indexer.py
@@ -0,0 +1,57 @@
+import pytest
+import os
+
+from core.modules.indexer import Indexer
+from core.db.products import Product_db
+from core.tests.fixtures.helper import gen_product, insert_products, gen_engine
+
+__author__ = 'asafe'
+
+
+@pytest.mark.parametrize("test_case", [
+    {
+        'products': [
+            gen_product(
+                _id=1,
+                url="url1",
+                status='PROCESSED',
+                title='title1',
+                name='name1',
+
+            ),
+            gen_product(
+                _id=2,
+                url="url2",
+                status='PROCESSED',
+                title='title2',
+                name='name2',
+
+            ),
+            gen_product(
+                _id=3,
+                url="url3",
+                status='PROCESSED',
+                title='title3',
+                name='name3',
+
+            )
+        ],
+        'expected': [
+            'nome_do_produto,título,url\n'
+            'name1,title1,url1\n'
+            'name2,title2,url2\n'
+            'name3,title3,url3\n'
+        ]
+    },
+])
+def test_export_data_to_csv(test_case, gen_engine):
+    insert_products(test_case['products'], gen_engine)
+    product_db = Product_db(gen_engine)
+    products = product_db.get_products_for_status('PROCESSED')
+    Indexer._test = True
+    Indexer.export_data_to_csv('test.csv', products)
+    with open('test.csv', 'r') as csvfile:
+        result = csvfile.read()
+
+    assert result == test_case['expected'][0]
+    os.remove('test.csv')
